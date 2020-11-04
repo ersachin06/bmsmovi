b67d795cd009e7a57b96b6ee239cc7f46ffe3d24
ersachin06 <er.sachin06@gmail.com>
2020-11-04 19:31:47 +0530

    seats selection

--- src/app/home-page/components/seats-selection/seats-selection.component.ts
+++ src/app/home-page/components/seats-selection/seats-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-seats-selection',
@@ -10,10 +10,11 @@ export class SeatsSelectionComponent implements OnInit {
 //   @Component({
 //     template: `<div *ngFor="let i of r">{{ i }}</div>`
 // })
-
+    @ViewChildren("img") hellos: QueryList<any>;
+  // @ViewChild('myId') myId: ElementRef;
    arrTtlGoldSeats:number[]= Array.from(Array(50).keys());
    arrTtlSilverSeats:number[]= Array.from(Array(25).keys());
-
+    seatImgUrl:string='../../../../assets/img/bseat.png';
   total:number=1000;
   showSeatList:any[]=[
     {seatNo:1,seatClass:'ist',fare:1000},
@@ -25,9 +26,17 @@ export class SeatsSelectionComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() 
+  {
+    console.log("view init fired start");
+    this.hellos.forEach(hello => console.log(hello));
+    console.log("view init fired end");
+  }
   Seat(x:any)
   {
 
+      alert('clicked '+ x);
+      this.seatImgUrl="../../../../assets/img/fseat.png";
   }
   confirmJourney()
   {
